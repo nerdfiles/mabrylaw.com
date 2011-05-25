e5e48f3d5346489d7a3dee05bc226c6f86447930
Aaron Alexander <nerdfiles@gmail.com>
2011-05-25 13:59:45 -0500

    wp customization

--- _wp/wp-content/themes/twentyten/rail-left.php
+++ _wp/wp-content/themes/twentyten/rail-left.php
@@ -4,7 +4,7 @@
                 
                 <div id="left-rail" class="rail columns-4 begin" role="complementary">
                     <div class="rail-inner">
-                        <p>I actually exist, both presentationally and in the source.</p>    
+                        <p>I actually exist, both presentationally and in the Source.</p>    
                     </div>
                 </div>
                 
