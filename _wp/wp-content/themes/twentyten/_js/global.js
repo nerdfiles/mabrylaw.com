5de327100e11f28abb291af146f49ffbd715c368
Aaron Alexander <nerdfiles@gmail.com>
2011-06-06 04:45:09 -0500

    cleanup

--- _wp/wp-content/themes/twentyten/_js/global.js
+++ _wp/wp-content/themes/twentyten/_js/global.js
@@ -3,10 +3,6 @@
  */
 
 $script.ready('jquery', function() {
-    
-// plugin init
-
-    $.metadata.setType("attr", "validate");
 
 // other stuff
 
@@ -15,6 +11,55 @@ $script.ready('jquery', function() {
 // domready
 
     $(document).ready(function() {
+        
+        // plugin init
+
+        $.metadata.setType("attr", "validate");
+        
+        // Wicked credit to
+        // http://www.zachstronaut.com/posts/2009/01/18/jquery-smooth-scroll-bugs.html
+        
+        var scrollElement = 'html, body';
+        
+        /*
+        $('html, body').each(function () {
+            var initScrollTop = $(this).attr('scrollTop');
+            $(this).attr('scrollTop', initScrollTop + 1);
+            if ($(this).attr('scrollTop') == initScrollTop + 1) {
+                scrollElement = this.nodeName.toLowerCase();
+                $(this).attr('scrollTop', initScrollTop);
+                return false;
+            }    
+        });
+        */
+        
+        // Smooth scrolling for internal links
+        
+        $("a[href^='#']").click(function(event) {
+            event.preventDefault();
+            
+            var $this = $(this),
+            target = this.hash,
+            $target = $(target);
+            
+            $(scrollElement).stop().animate({
+                'scrollTop': $target.offset().top
+            }, 500, 'swing', function() {
+                window.location.hash = target;
+            });
+            
+        });
+        
+        // ...
+        
+        /*
+        if ( $(window.location.hash).length ) {
+            $(scrollElement).stop().delay(700).animate({
+                'scrollTop': $(window.location.hash).offset().top
+            }, 2000);
+        }
+        */
+        
         
         // Set active page 
         
@@ -28,6 +73,12 @@ $script.ready('jquery', function() {
                 opacity: 1
             }, 200);
         });
+        
+        // Clean up
+        
+        $('a[title="Added with HTML Javascript Adder Wordpress plugin"]').animate({
+            opacity: .2
+        }, 100);
         
         // Set external
         
@@ -80,29 +131,69 @@ $script.ready('jquery', function() {
        
         $('#wpcf7-f1-p209-o1 form').attr('id', 'form_wpcf7-f1-p209-o1');  // Give Contact form id for plugin
         
-        // Inputs 
-        $('input[name="input_fullName"]').attr('validate', '{required:true, messages: { \
-            required: "Please provide your full name." \
-        }}');
-        $('input[name="input_email"]').attr('validate', '{required:true, email: true, messages: { \
-            required: "Please provide a valid e-mail address." \
-        }}');
-        $('input[name="input_subject"]').attr('validate', '{required:true, messages: { \
-            required: "What is your message about?" \
-        }}');
-        $('textarea[name="textarea_message"]').attr('validate', '{required:true, messages: { \
-            required: "What would you like to say?" \
-        }}');
+        // Inputs (introduced at...)
         
-        $('#wpcf7-f1-p209-o1 form').validate(); // Contact Form validation
+        $('input[name="input_fullName"]').attr('validate', '{required:true, messages: { required: "Please provide your full name." }}');
+        $('input[name="input_email"]').attr('validate', '{required:true, email: true, messages: { required: "Please provide a valid e-mail address." }}');
+        $('input[name="input_subject"]').attr('validate', '{required:true, messages: { required: "What is your message about?" }}');
+        $('textarea[name="textarea_message"]').attr('validate', '{required:true, messages: { required: "What would you like to say?" }}');
+        
+        $('#wpcf7-f1-p209-o1 form').validate({
+            submitHandler: function(form) {
+                
+                /*
+                var $form = $(form);
+                
+                $.ajax({
+                    type: "POST",
+                    url: $form.attr('action'),
+                    data: $form.serialize(),
+                    success: function(html){
+                        var $o = $(html).find('.wpcf7-response-output');
+                        $form.find('.wpcf7-response-output').remove();
+                        $form.prepend( $o );
+                        $(scrollElement).stop().delay(700).animate({
+                            'scrollTop': $('.wpcf7-response-output').offset().top
+                        }, 2000, 'swing');
+                    }
+                });
+                */
+               
+                $('body').stop().delay(700).animate({
+                    'scrollTop': 0
+                }, 2000, function() {
+                    form.submit();
+                });
+                
+            }
+        }); // Contact Form validation
         
         /* Request Legal Services Form validation */
         
-        $('#wpcf7-f1-p215-o1 form').attr('id', 'formwpcf7-f2-p215-o1 form') // Give Request Legal Services form id for plugin
+        $('#wpcf7-f2-p215-o1 form').attr('id', 'form_wpcf7-f2-p215-o1') // Give Request Legal Services form id for plugin
         
-        // Inputs
+        // Inputs (introduced at...)
         
-        $('#wpcf7-f1-p215-o1 form').validate(); // Request Legal Services Form validation
+        $('input[name="input_phone"]').attr('validate', '{required:true, phoneUS: true, messages: { required: "Please provide a contact number." }}');
+        $('input[name="input_documents"]').attr('validate', '{required:true, messages: { required: "Please provide some documentation to your case." }}');
+        
+        $('#wpcf7-f2-p215-o1 form').validate({
+            submitHandler: function(form) {
+                
+                /*
+                var $form = $(form),
+                    $form_clone = $form.clone();
+                    
+                $form_iframe = $('<iframe id="form_iframe">');
+                $('body').append($form_iframe);
+                $('#form_iframe').contents().find('body').append($form_clone);
+                $('#form_iframe').contents().find('form').submit();
+                */
+                
+                form.submit();
+                
+            }
+        }); // Request Legal Services Form validation
         
     });
 
@@ -139,6 +230,7 @@ $script.ready('jquery', function() {
     }
         
     $('#carousel ul').each(function(e) {
+        
         var carousel = $(this);
         var paused = false,
             $carouselItemVisible = $('.carousel-item-visible');
@@ -220,4 +312,3 @@ $script.ready('jquery', function() {
     });
 
 }); // global.js
-
