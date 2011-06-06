c8d5c38cfe3503d64fbcf1b4ce7a544d40d22e99
Aaron Alexander <nerdfiles@gmail.com>
2011-06-06 04:53:42 -0500

    cleanup

--- _wp/wp-content/themes/twentyten/_js/global.js
+++ _wp/wp-content/themes/twentyten/_js/global.js
@@ -21,18 +21,6 @@ $script.ready('jquery', function() {
         
         var scrollElement = 'html, body';
         
-        /*
-        $('html, body').each(function () {
-            var initScrollTop = $(this).attr('scrollTop');
-            $(this).attr('scrollTop', initScrollTop + 1);
-            if ($(this).attr('scrollTop') == initScrollTop + 1) {
-                scrollElement = this.nodeName.toLowerCase();
-                $(this).attr('scrollTop', initScrollTop);
-                return false;
-            }    
-        });
-        */
-        
         // Smooth scrolling for internal links
         
         $("a[href^='#']").click(function(event) {
@@ -72,6 +60,7 @@ $script.ready('jquery', function() {
             }, 100).delay(50).animate({
                 opacity: 1
             }, 200);
+            
         });
         
         // Clean up
@@ -138,35 +127,7 @@ $script.ready('jquery', function() {
         $('input[name="input_subject"]').attr('validate', '{required:true, messages: { required: "What is your message about?" }}');
         $('textarea[name="textarea_message"]').attr('validate', '{required:true, messages: { required: "What would you like to say?" }}');
         
-        $('#wpcf7-f1-p209-o1 form').validate({
-            submitHandler: function(form) {
-                
-                /*
-                var $form = $(form);
-                
-                $.ajax({
-                    type: "POST",
-                    url: $form.attr('action'),
-                    data: $form.serialize(),
-                    success: function(html){
-                        var $o = $(html).find('.wpcf7-response-output');
-                        $form.find('.wpcf7-response-output').remove();
-                        $form.prepend( $o );
-                        $(scrollElement).stop().delay(700).animate({
-                            'scrollTop': $('.wpcf7-response-output').offset().top
-                        }, 2000, 'swing');
-                    }
-                });
-                */
-               
-                $('body').stop().delay(700).animate({
-                    'scrollTop': 0
-                }, 2000, function() {
-                    form.submit();
-                });
-                
-            }
-        }); // Contact Form validation
+        $('#wpcf7-f1-p209-o1 form').validate(); // Contact Form validation
         
         /* Request Legal Services Form validation */
         
@@ -177,23 +138,7 @@ $script.ready('jquery', function() {
         $('input[name="input_phone"]').attr('validate', '{required:true, phoneUS: true, messages: { required: "Please provide a contact number." }}');
         $('input[name="input_documents"]').attr('validate', '{required:true, messages: { required: "Please provide some documentation to your case." }}');
         
-        $('#wpcf7-f2-p215-o1 form').validate({
-            submitHandler: function(form) {
-                
-                /*
-                var $form = $(form),
-                    $form_clone = $form.clone();
-                    
-                $form_iframe = $('<iframe id="form_iframe">');
-                $('body').append($form_iframe);
-                $('#form_iframe').contents().find('body').append($form_clone);
-                $('#form_iframe').contents().find('form').submit();
-                */
-                
-                form.submit();
-                
-            }
-        }); // Request Legal Services Form validation
+        $('#wpcf7-f2-p215-o1 form').validate(); // Request Legal Services Form validation
         
     });
 
@@ -312,3 +257,4 @@ $script.ready('jquery', function() {
     });
 
 }); // global.js
+
